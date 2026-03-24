abbf5189f913106135a3e224aba22b8799c05c21
JVRFurtado <joao.furtado546@gmail.com>
2026-03-24 09:41:29 -0300

    Adição do uso do LocalStora para teste persistente

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// Acessa o LocalStorage para armzenar os dados
+const contatosStorage = JSON.parse(localStorage.getItem("contatos")) || [];
+
+document.getElementById('formCadastro').addEventListener('submit', function(e) {
+    e.preventDefault(); // Impede o recarregamento da página
+});
+
 // Coleta dados do formulário
 const nome = document.getElementById('nome').value;
 const departamento = document.getElementById('departamento').value;
@@ -10,7 +17,12 @@ const novoContato = {
     ramal,
 };
 
+// Função para exibir os contatos na tabela
 function exibirContatos() {
     const tabela = document.getElementById('tabelaContatos').getElementsByTagName('tbody')[0];
-    tabela.innerHTML = '';
+    tabela.innerHTML = ''; // Limpa a tabela antes de preencher
+}
+
+function removerContato(index) {
+    
 }
