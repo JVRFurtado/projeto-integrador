da18b5566506821a92fcd228c6024bdfafb775e0
JVRFurtado <joao.furtado546@gmail.com>
2026-03-24 09:25:39 -0300

    Adição de uma função para exibir os contatos na tabela (function exibirContatos)

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const nome = document.getElementById('nome').value;
 const departamento = document.getElementById('departamento').value;
 const ramal = document.getElementById('ramal').value;
 
+// Cria um novo contato
 const novoContato = {
     nome,
     departamento,
     ramal,
 };
+
+function exibirContatos() {
+    const tabela = document.getElementById('tabelaContatos').getElementsByTagName('tbody')[0];
+    tabela.innerHTML = '';
+}
