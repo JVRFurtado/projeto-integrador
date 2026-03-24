82857b7cf6c027a04825096c78b8368852884d88
JVRFurtado <joao.furtado546@gmail.com>
2026-03-24 00:26:11 -0300

    Criação do arquivo JavaScript e criação de constantes que coletam dados do formulário

---
+++ app.js
@@ -0,0 +1,4 @@
+// Coleta dados do formulário
+const nome = document.getElementById('nome').value;
+const departamento = document.getElementById('departamento').value;
+const ramal = document.getElementById('ramal').value;
