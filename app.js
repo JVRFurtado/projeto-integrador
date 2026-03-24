ee6891aff53177052876f53d9e6d1a40930cc1fe
JVRFurtado <joao.furtado546@gmail.com>
2026-03-24 09:19:29 -0300

    Adição de um método de criação de objeto "contato" (const novoContato)

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 const nome = document.getElementById('nome').value;
 const departamento = document.getElementById('departamento').value;
 const ramal = document.getElementById('ramal').value;
+
+const novoContato = {
+    nome,
+    departamento,
+    ramal,
+};
